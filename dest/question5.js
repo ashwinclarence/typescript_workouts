a7592d2d44f02c347ae2ba8290310f7e0164c8be
ASHWIN <meashwinclarence@gmail.com>
2024-08-16 11:30:17 +0530

    q5

---
+++ dest/question5.js
@@ -0,0 +1,22 @@
+"use strict";
+function gradeDisplay(mark) {
+    if (mark > 90) {
+        return "A";
+    }
+    else if (mark > 80) {
+        return "B";
+    }
+    else if (mark > 70) {
+        return "C";
+    }
+    else if (mark > 60) {
+        return "D";
+    }
+    else if (mark > 50) {
+        return "E";
+    }
+    else {
+        return "F";
+    }
+}
+console.log(gradeDisplay(56));
