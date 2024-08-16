6388c8f3ee4816f02f302f38a16a3f7f0d6604d9
ASHWIN <meashwinclarence@gmail.com>
2024-08-16 11:12:06 +0530

    q4

---
+++ dest/question4.js
@@ -0,0 +1,8 @@
+"use strict";
+let mark = 34;
+if (mark > 50) {
+    console.log("You passed the exam");
+}
+else {
+    console.log("You failed in the exam");
+}
---
+++ src/question4.ts
@@ -0,0 +1,7 @@
+let mark: number = 34;
+
+if (mark > 50) {
+    console.log("You passed the exam")
+} else {
+    console.log("You failed in the exam")
+}
