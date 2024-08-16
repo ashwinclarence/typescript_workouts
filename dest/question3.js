239d46a0b174a2c3186ac8b73d60e12f9f58a227
ASHWIN <meashwinclarence@gmail.com>
2024-08-16 11:09:47 +0530

    q3

---
+++ dest/question3.js
@@ -0,0 +1,6 @@
+"use strict";
+let principalAmount = 2000;
+let interestRate = 5;
+let numberOfYears = 10;
+let simpleInterest = (principalAmount * interestRate * numberOfYears) / 100;
+console.log("simple interest is ", simpleInterest);
---
+++ src/question3.ts
@@ -0,0 +1,7 @@
+let principalAmount: number = 2000;
+let interestRate: number = 5;
+let numberOfYears: number = 10;
+
+let simpleInterest: number = (principalAmount * interestRate * numberOfYears) / 100;
+
+console.log("simple interest is ",simpleInterest)
