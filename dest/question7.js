63ac3f1b684f53f49e3aaa6d6e633276b190e087
ASHWIN <meashwinclarence@gmail.com>
2024-08-16 11:43:41 +0530

    q7

---
+++ dest/question7.js
@@ -0,0 +1,7 @@
+"use strict";
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i} X ${num} = ${i * num}`);
+    }
+}
+multiplicationTable(5);
---
+++ src/question7.ts
@@ -0,0 +1,7 @@
+function multiplicationTable(num: number) {
+    for (let i = 1; i <= 10; i++){
+        console.log(`${i} X ${num} = ${i * num}`);
+    }
+}
+
+multiplicationTable(5)
